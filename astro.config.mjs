f82d9c7153f440559a67ca8c3d485d8b7da4bb93
Bonfry <info@bonfry.com>
2024-09-03 21:39:35 +0200

    removed AstroPWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,48 +30,5 @@ export default defineConfig({
     tailwind(),
     robotsTxt(),
     react(),
-    AstroPWA({
-      workbox: { navigateFallback: "/404" },
-      mode: 'production',
-      base: '/',
-      scope: '/',
-      includeAssets: ['favicon.svg'],
-      registerType: 'autoUpdate',
-      manifest: {
-        name: WebsiteConfig.DEVFEST_NAME,
-        short_name: WebsiteConfig.DEVFEST_NAME,
-        theme_color: WebsiteConfig.DEVFEST_THEME_COLOR,
-        icons: [
-          {
-            src: 'assets/images/icons/icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/images/icons/icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/images/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/images/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ],
-      },
-      workbox: {
-        navigateFallback: '/',
-        globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
-      },
-      experimental: {
-        directoryAndTrailingSlashHandler: true,
-      }
-    }),
   ],
 });
--- dev.astro.config.mjs
+++ dev.astro.config.mjs
@@ -26,41 +26,5 @@ export default defineConfig({
       applyBaseStyles: false,
     }),
     react(),
-    AstroPWA({
-      mode:'development',
-      base: '/',
-      scope: '/',
-      includeAssets: ['favicon.svg'],
-      manifest: {
-        name: WebsiteConfig.DEVFEST_NAME,
-        short_name: WebsiteConfig.DEVFEST_NAME,
-        theme_color: WebsiteConfig.DEVFEST_THEME_COLOR,
-        icons: [
-          {
-            src: 'assets/images/icons/icon-72x72.png',
-            sizes: '72x72',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/images/icons/icon-144x144.png',
-            sizes: '144x144',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/images/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'assets/images/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ],
-      },
-
-
-    }),
   ],
 });
