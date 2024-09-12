b195e8756ecc69f097da19a6660c35e06d1c2f16
Domingo Dirutigliano <me@domy.sh>
2024-09-12 20:13:02 +0200

    fix: token as queryString due to astro limitations

--- src/react/PageComponent.tsx
+++ src/react/PageComponent.tsx
@@ -1,23 +1,33 @@
+import { useEffect } from "react";
 import { AppMain } from "./AppMain";
 import { LoginPage } from "./pages/LoginPage";
 import { RegsitrationPage } from "./pages/RegistrationPage";
 
-export type PageData = { page: "login" } | { page: "registration", token: string } | { page: "null" }
+export type PageData = { page: "login" } | { page: "registration"} | { page: "null" }
 
 export const PageComponent = (props:PageData) => {
 
+    const registrationToken = new URLSearchParams(location.search).get("token")
     
     const pageComponent = () => {
         if(props.page === "login") {
             return <LoginPage />
         }
-        if(props.page === "registration") {
-            return <RegsitrationPage token={props.token} />
+        if(props.page === "registration" && registrationToken) {
+            return <RegsitrationPage token={registrationToken} />
         }
         return null
     }
+
+    const content = pageComponent()
+
+    useEffect(() => {
+        if(!content) {
+            location.href = "/"
+        }
+    }, [content])
     
     return <AppMain>
-        {pageComponent()}
+        {content}
     </AppMain>
 }
--- src/react/pages/RegistrationPage.tsx
+++ src/react/pages/RegistrationPage.tsx
@@ -1,8 +1,10 @@
 import { AppMain } from "../AppMain";
 
-export const RegsitrationPage = ({ token }:{
-    token: string
-}) => {
+export const RegsitrationPage = ({ token }: { token:string }) => {
+
+    if(!token) {
+        return <AppMain>Invalid token</AppMain>
+    }
     return (
         <AppMain>
             registration token: {token}
