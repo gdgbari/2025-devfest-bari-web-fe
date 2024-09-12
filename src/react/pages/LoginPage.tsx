4388f236ede511d3cbbd97c9d0fbd408fa0cbd56
Domingo Dirutigliano <me@domy.sh>
2024-09-12 20:55:41 +0200

    fix: mantine form fix behaviour

--- src/react/pages/LoginPage.tsx
+++ src/react/pages/LoginPage.tsx
@@ -1,10 +1,10 @@
 import { AppMain } from "../AppMain";
-import { Button, Card, Input } from "react-daisyui"
+import { Card, Input } from "react-daisyui"
 import { useForm } from "@mantine/form"
-import Jumbo from "../../components/Home/Jumbo.astro";
 import { WebsiteConfig } from "../../config";
 import { useTranslations } from "../../i18n/utils";
 import { showNotification } from "@mantine/notifications";
+import { isEmailValid } from "../utils";
 
 export const LoginPage = () => {
     const form = useForm({
@@ -13,22 +13,35 @@ export const LoginPage = () => {
             password: ""
         },
         validate: {
-            email: (value) => value.length > 0,
-            password: (value) => value.length > 0
-        }
+            email: (value) => value.length > 0? isEmailValid(value)? undefined: "The email given is invalid": "Email is required",
+            password: (value) => value.length > 0? undefined: "Password is required"
+        },
+        validateInputOnChange: true
+        
     })
 
     const t = useTranslations("en")
 
+    const checkErrors = () => {
+        if (!form.isValid()) {
+            showNotification({
+                title: "Form error",
+                message: Object.values(form.errors).join(", "),
+                color: "red"
+            })
+        }
+    }
+
     return (
         <AppMain>
             <form onSubmit={form.onSubmit((data)=>{
-                showNotification({
-                    title: "Function not implemented",
-                    message: JSON.stringify(data),
-                    color: "blue"
+                    showNotification({
+                        title: "Function not implemented",
+                        message: JSON.stringify(data),
+                        color: "blue"
+                    })
                 })
-            })}>
+            }>
                 <Card className="md:px-20 md:py-16 bg-black bg-opacity-80 z-10 md:h-fit md:w-fit h-screen w-screen justify-center rounded-none px-5 md:rounded-xl">
                     <div className="flex flex-col opacity-100 justify-center items-center">
                         <img src="/assets/vectors/logo_big.svg" className="h-36 m-2 px-8 md:p-0 md:m-6" />
@@ -47,7 +60,7 @@ export const LoginPage = () => {
                             <Input placeholder="Password" className="mt-3" {...form.getInputProps("password")} />
                         </div>
                         <div className="flex flex-col mt-10 items-center w-full">
-                        <Input type="submit" value="Login" className="btn btn-primary btn-wide" />
+                        <Input type="submit" value="Login" className="btn btn-primary btn-wide" onClick={checkErrors} />
                         </div>
                     </div>
                 </Card>
