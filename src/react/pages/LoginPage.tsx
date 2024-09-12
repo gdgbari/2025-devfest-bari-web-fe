9cb551cc4e028afe5f0ef83dea31396066632777
Domingo Dirutigliano <me@domy.sh>
2024-09-12 20:25:22 +0200

    fix: mobile view

--- src/react/pages/LoginPage.tsx
+++ src/react/pages/LoginPage.tsx
@@ -29,9 +29,9 @@ export const LoginPage = () => {
                     color: "blue"
                 })
             })}>
-                <Card className="px-20 py-16 bg-black bg-opacity-80 z-10">
+                <Card className="md:px-20 md:py-16 bg-black bg-opacity-80 z-10 md:h-fit md:w-fit h-screen w-screen justify-center rounded-none px-5 md:rounded-xl">
                     <div className="flex flex-col opacity-100 justify-center items-center">
-                        <img src="/assets/vectors/logo_big.svg" className="h-36 m-2 md:m-6" />
+                        <img src="/assets/vectors/logo_big.svg" className="h-36 m-2 px-8 md:p-0 md:m-6" />
                         <p className="text-2xl md:text-3xl font-semibold mt-5">
                             {
                             WebsiteConfig.EVENT_START.toLocaleDateString("en", {
@@ -42,11 +42,11 @@ export const LoginPage = () => {
                             }
                         </p>
                         <p className="text-2xl md:text-3xl font-semibold">{t("info.locationName")}</p>
-                        <div className="flex flex-col mt-10" style={{ width: "100%"}}>
+                        <div className="flex flex-col mt-10 w-full">
                             <Input placeholder="Email" {...form.getInputProps("email")} />
                             <Input placeholder="Password" className="mt-3" {...form.getInputProps("password")} />
                         </div>
-                        <div className="flex flex-col mt-10 items-center" style={{ width: "100%"}}>
+                        <div className="flex flex-col mt-10 items-center w-full">
                         <Input type="submit" value="Login" className="btn btn-primary btn-wide" />
                         </div>
                     </div>
