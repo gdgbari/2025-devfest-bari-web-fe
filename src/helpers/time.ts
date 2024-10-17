324c300bea7acfa17c085c6f3f0e10f004e0eb4c
Domingo Dirutigliano <me@domy.sh>
2024-10-17 07:43:27 +0200

    fix: time start-end timezone

--- src/helpers/time.ts
+++
@@ -1,9 +0,0 @@
-export function formatTime(date: Date): string {
-    const hours = date.getHours();
-    const hh = hours > 9 ? hours.toString() : `0${hours}`;
-
-    const minutes = date.getMinutes();
-    const mm = minutes > 9 ? minutes.toString() : `0${minutes}`;
-    
-    return `${hh}:${mm}`;
-}
