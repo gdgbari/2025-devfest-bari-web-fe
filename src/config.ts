81df0d12222ffe3836be2febaf7446b63e157e41
Domingo Dirutigliano <me@domy.sh>
2025-11-14 19:55:24 +0100

    releasing schedule and some fixes

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export class WebsiteConfig {
     /*--------------------------------------------
     |              GENERAL SETTINGS              |  
     --------------------------------------------*/
-    
+
     public static readonly DEVFEST_NAME: string = 'Devfest Bari 2025';
     public static readonly DEVFEST_LOGO_LIGHT: string = '/assets/images/logo_light.webp';
     public static readonly DEVFEST_LOGO_DARK: string = '/assets/images/logo_dark.webp';
@@ -16,49 +16,49 @@ export class WebsiteConfig {
         "dev.devfest.it",
         "gdg.community.dev"
     ]
-    public static readonly SHOW_EVENT_TALKS: boolean = false;
+    public static readonly SHOW_EVENT_TALKS: boolean = true;
     public static readonly DEVFEST_THEME_COLOR: string = '#f59e0b';
 
     /*--------------------------------------------
     |              EVENT INFO                    |  
     --------------------------------------------*/
 
-    public static readonly EVENT_START : Date = new Date("2025-11-29T09:00:00+01:00");
-    public static readonly EVENT_END : Date = new Date("2025-11-29T19:00:00+01:00");
-    public static readonly EVENT_TIMEZONE : string = 'Europe/Rome';
-    public static readonly EVENT_LOCATION_NAME_ITA : String = 'Politecnico di Bari';
-    public static readonly EVENT_LOCATION_NAME_ENG : String = 'Polythecnic of Bari';
-    public static readonly EVENT_LOCATION_CITY : String = 'Bari';
-    public static readonly EVENT_LOCATION_ADDRESS : String = 'Via Edoardo Orabona 4';
-    public static readonly EVENT_LOCATION_FULL_ADDRESS : String = ` ${this.EVENT_LOCATION_ADDRESS}, 70126 Bari BA`;
+    public static readonly EVENT_START: Date = new Date("2025-11-29T09:00:00+01:00");
+    public static readonly EVENT_END: Date = new Date("2025-11-29T19:00:00+01:00");
+    public static readonly EVENT_TIMEZONE: string = 'Europe/Rome';
+    public static readonly EVENT_LOCATION_NAME_ITA: String = 'Politecnico di Bari';
+    public static readonly EVENT_LOCATION_NAME_ENG: String = 'Polythecnic of Bari';
+    public static readonly EVENT_LOCATION_CITY: String = 'Bari';
+    public static readonly EVENT_LOCATION_ADDRESS: String = 'Via Edoardo Orabona 4';
+    public static readonly EVENT_LOCATION_FULL_ADDRESS: String = ` ${this.EVENT_LOCATION_ADDRESS}, 70126 Bari BA`;
 
-    
+
     /*--------------------------------------------
     |              SOCIAL SHARE                  |  
     --------------------------------------------*/
 
-    public static readonly TWITTER_SOCIAL_TEXT : string = 'Devfest Bari 2025 {url}';
-    public static readonly TELEGRAM_MESSAGE_TEXT : string = 'Devfest Bari 2025';
+    public static readonly TWITTER_SOCIAL_TEXT: string = 'Devfest Bari 2025 {url}';
+    public static readonly TELEGRAM_MESSAGE_TEXT: string = 'Devfest Bari 2025';
 
     /*--------------------------------------------
     |              SOCIAL PAGES                  |  
     --------------------------------------------*/
 
-    public static readonly FACEBOOK_PAGE : string = 'https://www.facebook.com/GDGBari';
-    public static readonly MEETUP_PAGE : string = '';
-    public static readonly INSTAGRAM_PAGE : string = 'https://www.instagram.com/gdgbari/';
-    public static readonly LINKEDIN_PAGE : string = 'https://www.linkedin.com/company/gdgbari/';
-    public static readonly TELEGRAM_SPEAKERS_GROUP : string = 'https://t.me/+FFIAMECGZGVjNmU0';
-    public static readonly WEBSITE : string = 'https://gdg.community.dev/gdg-bari/';
+    public static readonly FACEBOOK_PAGE: string = 'https://www.facebook.com/GDGBari';
+    public static readonly MEETUP_PAGE: string = '';
+    public static readonly INSTAGRAM_PAGE: string = 'https://www.instagram.com/gdgbari/';
+    public static readonly LINKEDIN_PAGE: string = 'https://www.linkedin.com/company/gdgbari/';
+    public static readonly TELEGRAM_SPEAKERS_GROUP: string = 'https://t.me/+FFIAMECGZGVjNmU0';
+    public static readonly WEBSITE: string = 'https://gdg.community.dev/gdg-bari/';
 
     /*--------------------------------------------
     |              NUMBERS INFO                  |  
     --------------------------------------------*/
 
-    public static readonly NUM_PARTECIPENTS : number = 700;
-    public static readonly NUM_TRACKS_FALLBACK : number = 7;
+    public static readonly NUM_PARTECIPENTS: number = 700;
+    public static readonly NUM_TRACKS_FALLBACK: number = 7;
 
-    public static readonly TALKS_SLIDE_DOWNLOAD_LINK : string = '';
+    public static readonly TALKS_SLIDE_DOWNLOAD_LINK: string = '';
 
     public static readonly SESSIONIZE_API_KEY = "i9otum6s";
 
